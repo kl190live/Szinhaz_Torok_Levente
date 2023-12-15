4c7066741428e0f70d48a2e3acceb5f8313cff60
Petrik Diak <diak@petrik.hu>
2023-12-15 10:22:25 +0100

    A navbar belerakasa es a post majnem kesz

---
+++ src/UjKupon.Dto.ts
@@ -0,0 +1,5 @@
+export class KuponDto{
+  title:string;
+  percentage:number;
+  code:string;
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import * as mysql from 'mysql2';
 import { AppService } from './app.service';
+import { KuponDto } from './UjKupon.Dto';
+import { Response } from 'express';
 
 const conn = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
@@ -25,4 +27,49 @@ export class AppController {
       kuponok:adatok 
     };
   }
+
+  @Get('/ujKupon')
+  @Render('ujKuponMegadas')
+  ujKuponForm() {
+    return { errors: [] };
+  }
+
+  @Post('/ujKupon')
+  @Render('ujKuponMegadas')
+  async ujKupon(@Body() ujKupon: KuponDto, @Res() res:Response) {
+    const errors: string[] = [];
+    const kuponCode=/[a-zA-Z]{4}-[0-9]{6}/;
+    if(ujKupon.title == null){
+      errors.push('Az előadás címét meg kell adni');
+    }
+    if( 1<ujKupon.percentage || ujKupon.percentage<100){
+      errors.push('A kedvezmény csak 1-99 között lehet megadni');
+    }
+    if(ujKupon.percentage == null){
+      errors.push('A kedvezményt meg kell adni');
+    }
+    if(ujKupon.code == null){
+      errors.push('A kupon kódját meg kell andi');
+    }
+    if(!kuponCode.test(ujKupon.code)){
+      errors.push('A kupon 11 karakterből kell álnia és a példa szerint kell kinéznie pl: DFRW-764332');
+    }
+    
+    const title = ujKupon.title;
+    const percentage : number= ujKupon.percentage;
+    const code = ujKupon.code;
+
+    
+    if(errors.length > 0){
+      return {
+        errors,
+      };
+    }
+    else{
+      const [ adatok ] = await conn.execute('INSERT INTO kuponok (title, percentage, code) VALUES (?, ?, ?)',[title, percentage, code],);
+      console.log(adatok);
+      res.redirect('/');
+    }
+  }
+
 }
